d9f6574ec7aa2d5c9cd8af9923a480d48de035c9
Rodrigo Leutz <rodrigo@sempreupdate.eu>
2019-09-28 15:17:37 -0300

    Adicionada função de ler notas.

--- media_alunos.kt
+++ media_alunos.kt
@@ -1,5 +1,3 @@
-import java.util.Scanner
-
 fun main(){ 
 
 	var nome: String?
@@ -9,20 +7,39 @@ fun main(){
 	var n4: Double
 	var media: Double
 
-	var reader = Scanner(System.`in`) 
-
 	println("Digite o nome do aluno:")
 	nome = readLine()
 	println("Digite a nota 1:") 
-	n1 = reader.nextDouble() 
+	n1 = readNota()
 	println("Digite a nota 2:") 
-	n2 = reader.nextDouble() 
+	n2 = readNota()
 	println("Digite a nota 4:") 
-	n3 = reader.nextDouble() 
+	n3 = readNota()
 	println("Digite a nota 4:") 
-	n4 = reader.nextDouble() 
+	n4 = readNota()
 
 	media = (n1+n2+n3+n4)/4
-	println("\nNome: "+nome)
+	println("\nNome: $nome")
 	println("Média: $media") 
+}
+private fun readNota(): Double {
+	var nota = readLine()
+	try{
+		nota = nota!!.replace(",",".")
+		val y = nota.toDouble()
+		if(y > 10.0) {
+				println("Nota maior que 10, valor = 10.")
+				return 10.0
+		}
+		else if(y < 0.0) {
+				println("Nota menor que 0, valor = 0.")
+				return 0.0
+		}
+		else {
+				return y
+		}
+	} catch(e: NumberFormatException){
+		println("Não foi digitado um número válido, valor = 0.")
+		return 0.0
+	}
 }
