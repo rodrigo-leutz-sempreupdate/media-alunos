9c074423a0a6cd702de3132c063ba27d3343d526
Rodrigo Leutz <rodrigo@sempreupdate.eu>
2019-09-29 16:55:18 -0300

    Criando data class e deixando em funções

--- media_alunos.kt
+++ media_alunos.kt
@@ -1,45 +1,53 @@
 fun main(){ 
 
-	var nome: String?
-	var n1: Double
-	var n2: Double
-	var n3: Double
-	var n4: Double
-	var media: Double
+        var aluno: Aluno = readAluno()
+        showMedia(aluno) 
 
-	println("Digite o nome do aluno:")
-	nome = readLine()
-	println("Digite a nota 1:") 
-	n1 = readNota()
-	println("Digite a nota 2:") 
-	n2 = readNota()
-	println("Digite a nota 4:") 
-	n3 = readNota()
-	println("Digite a nota 4:") 
-	n4 = readNota()
-
-	media = (n1+n2+n3+n4)/4
-	println("\nNome: $nome")
-	println("Média: $media") 
+}
+private fun readAluno(): Aluno {
+        println("Digite o nome do aluno:")
+        val nome = readLine()
+        println("Digite a nota 1:") 
+        val n1 = readNota()
+        println("Digite a nota 2:") 
+        val n2 = readNota()
+        println("Digite a nota 4:")
+        val n3 = readNota()
+        println("Digite a nota 4:")
+        val n4 = readNota()
+        return Aluno(nome,n1,n2,n3,n4)
+}
+private fun showMedia(aluno: Aluno) {
+        val media = (aluno.n1+aluno.n2+aluno.n3+aluno.n4)/4
+        println("\nNome: "+aluno.nome)
+        println("Média: $media")
 }
 private fun readNota(): Double {
-	var nota = readLine()
-	try{
-		nota = nota!!.replace(",",".")
-		val y = nota.toDouble()
-		if(y > 10.0) {
-				println("Nota maior que 10, valor = 10.")
-				return 10.0
-		}
-		else if(y < 0.0) {
-				println("Nota menor que 0, valor = 0.")
-				return 0.0
-		}
-		else {
-				return y
-		}
-	} catch(e: NumberFormatException){
-		println("Não foi digitado um número válido, valor = 0.")
-		return 0.0
-	}
+        var nota = readLine()
+        try{
+                nota = nota!!.replace(",",".")
+                val y = nota.toDouble()
+                if(y > 10.0) {
+                                println("Nota maior que 10, valor = 10.")
+                                return 10.0
+                }
+                else if(y < 0.0) {
+                                println("Nota menor que 0, valor = 0.")
+                                return 0.0
+                }
+                else {
+                                return y
+                }
+        } catch(e: NumberFormatException){
+                println("Não foi digitado um número válido, valor = 0.")
+                return 0.0
+        }
 }
+data class Aluno(
+        var nome: String?,
+        var n1: Double = 0.0,
+        var n2: Double = 0.0,
+        var n3: Double = 0.0,
+        var n4: Double = 0.0
+)
+
